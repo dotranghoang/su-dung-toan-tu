1b8c8382485104398983c4535966feeafc93db1d
Do-Trang-Hoang <dotranghoang@gmail.com>
2019-07-25 09:49:34 +0700

    chu vi

---
+++ chuvi.js
@@ -0,0 +1,7 @@
+let inputWidth;
+let inputHeight;
+inputWidth=prompt('Nhap chieu dai','0');
+inputHeight=prompt('nhap chieu rong','0');
+let area;
+area=inputWidth*inputHeight;
+document.write('Chu Vi La ' + area);
