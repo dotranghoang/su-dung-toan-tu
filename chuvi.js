0a6c01a16c8950d57f82c07192f705f6e6364d70
Do-Trang-Hoang <dotranghoang@gmail.com>
2019-07-25 10:04:20 +0700

    Thuc Hanh Toan Tu

--- chuvi.js
+++ chuvi.js
@@ -4,9 +4,9 @@ inputWidth=parseFloat(prompt('Nhập Độ Dài','0'));
 inputHeight=parseFloat(prompt('Nhập Độ Rộng','0'));
 let area;
 area=inputWidth*inputHeight;
-document.write('Độ Dài : '+ inputWidth);
+document.write('Độ Dài : <br/> '+ inputWidth);
 document.write('<br/>');
-document.write('Độ Rộng : '+ inputHeight);
+document.write('Độ Rộng : <br/>'+ inputHeight);
 document.write('<br/>');
 
 document.write('Chu Vi Là : ' + area);
